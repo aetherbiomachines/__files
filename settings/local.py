5536f24ca79dae8a5401b7d5da15daad79cbba58
Alberto Garcia <alberto@aetherbio.com>
2021-12-28 20:50:36 -0800

    Add silk to local settings

--- settings/local.py
+++ settings/local.py
@@ -3,6 +3,10 @@ from settings import *
 DEBUG = True
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
+MIDDLEWARE.append('silk.middleware.SilkyMiddleware')
+INSTALLED_APPS.append('silk')
+SILKY_PYTHON_PROFILER = True
+
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.postgresql_psycopg2',
